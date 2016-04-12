790b6423fc921ec8f492114bfbbe0d1d2f4afbab
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2016-04-12 18:26:39 -0300

    Change routes and add a new module of controllers

--- client/app.js
+++ client/app.js
@@ -1,17 +1,28 @@
 'use strict'
 
-var drugmanApp = angular.module('Drugman',['ngRoute', 'drugstoreCtrls']);
+var drugmanApp = angular.module('Drugman',['ngRoute', 'drugstoreCtrls', 'drugCtrl']);
 
 drugmanApp.config(['$routeProvider',function($routeProvider){
   $routeProvider.when('/',{
+    redirectTo: '/drugstore',
+  }).
+  when('/drugstore',{
     templateUrl: 'views/drugstore/list.html',
     controller: 'ListDSCtrl',
+  }).
+  when('/drugstore/:id',{
+    templateUrl: 'views/drugstore/single.html',
+    controller: 'SingleDSCtrl',
   }).
   when('/drug',{
     templateUrl: 'views/drug/list.html',
     controller: 'ListDrugCtrl',
   }).
+  when('/drug/:id',{
+    templateUrl: 'views/drug/single.html',
+    controller: 'SingleDrugCtrl',
+  }).
   otherwise({
-    redirectTo: '/',
+    redirectTo: '/drugstore',
   });
 }]);
