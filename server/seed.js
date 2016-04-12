0e6a9b424393a70402c943922663eeb452496406
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2016-04-12 10:22:16 -0300

    Connect server to db and create seed of samples
    The connection with mongodb is created with default configuration, which means
    default port and user.
    The seed creates 2 drugstores, 8 drugs( the 8 most selled drugs in Brazil) and
    add 2 drugs to the first drugstore.

---
+++ server/seed.js
@@ -0,0 +1,87 @@
+'use strict'
+
+var Drug = require('./models/drug.js'),
+    Drugstore = require('./models/drugstore.js'),
+    DrugstoreDrug = require('./models/drugstore_drug.js');
+
+function createCallback(err){
+  if (err) {
+    console.log(err);
+  }
+  console.log("Creation sucessful");
+}
+
+function clean(){
+  // Remove all drugstores.
+  Drugstore.remove(function(err){
+    if(err){
+      console.log(err);
+    }
+    console.log("Drugstores has been succcessfully deleted");
+  });
+
+  // Remove all drugs.
+  Drug.remove(function(err){
+    if (err) {
+      console.log(err);
+    }
+    console.log("Drugs has been Successfully deleted");
+  });
+
+  DrugstoreDrug.remove(function(err){
+    if(err){
+      console.log(err);
+    }
+    console.log("DrugstoreDrugs has been succcessfully deleted");
+  });
+}
+
+function seed(){
+
+  // Cleaning database
+  clean();
+
+  // Create drugstores
+  var drogasil = new Drugstore({name: 'Drogasil'}),
+      menorPreco = new Drugstore({name: 'Menor Preço'});
+
+  // Create Drugs
+  var neosoro = new Drug({name: 'Neosoro'}),
+      puran = new Drug({name: 'Puran t4'}),
+      salonpas = new Drug({name: 'Salonpas'}),
+      cliclo = new Drug({name: 'Cliclo 21'}),
+      microvlar = new Drug({name: 'Microvlar'}),
+      buscopan = new Drug({name: 'Buscopan'}),
+      rivotril = new Drug({name: 'Rivotril'}),
+      dorflex = new Drug({name: 'Dorflex'});
+
+  var drogd1 = new DrugstoreDrug({
+        drugstoreId: drogasil._id,
+        drugId: neosoro._id,
+        stock: 100,
+        price: 10.0,
+      }),
+      drogd2 = new DrugstoreDrug({
+        drugstoreId: drogasil._id,
+        drugId: salonpas._id,
+        stock: 10,
+        price: 20.0,
+      });
+
+  drogasil.save(createCallback);
+  menorPreco.save(createCallback);
+
+  neosoro.save(createCallback);
+  puran.save(createCallback);
+  salonpas.save(createCallback);
+  cliclo.save(createCallback);
+  microvlar.save(createCallback);
+  buscopan.save(createCallback);
+  rivotril.save(createCallback);
+  dorflex.save(createCallback);
+
+  drogd1.save(createCallback);
+  drogd2.save(createCallback);
+}
+
+module.exports = seed;
--- server/server.js
+++ server/server.js
@@ -1,24 +1,36 @@
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
     drugstoreCtrl = require('./controllers/drugstore.js'),
-    drugCtrl = require('./controllers/drug.js');
+    drugCtrl = require('./controllers/drug.js'),
+    seed = require('./seed.js');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Set public folder.
 app.use(express.static('../client'));
 
+// Connect with db with default config.
+mongoose.connect('mongodb://localhost/drugman');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open',function(){
+  console.log("Connection with db is open.");
+});
+
+// Seed db.
+seed();
+
+// Middleware used in every request.
 app.use(function timelog(req, res, next){
   console.log("Time : "+Date.now());
   next();
 });
 
-app.get('/',function(req, res){
-  console.log('Root');
-  res.render('index.html');
-});
-
 app.use('/drugstore',drugstoreCtrl);
 app.use('/drug',drugCtrl);
-
 
 app.listen(3000, function(){
   console.log("Listening at port 3000");
